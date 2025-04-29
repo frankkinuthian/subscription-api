6463f3dc6ea88448eea0cd85d300219114e50279
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-04-30 01:00:05 +0300

    SignUp controller logic with JWT complete

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { signIn, signOut, signUp } from "../controllers/auth.controller.js";
 
+
 const authRouter = Router();
 
 authRouter.post('/sign-up', signUp)
