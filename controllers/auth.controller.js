9c34ba15e856a8d7fea09c4048919bcb7f6335a7
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-04-30 00:28:52 +0300

    Begin controllers logic

---
+++ controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signUp = async (req, res, next) => {
+    // Signup logic here
+}
+
+export const signIn = async (req, res, next) => {
+    // Signin logic here
+}
+
+export const signOut = async (req, res, next) => {
+    // Signout logic here
+}
